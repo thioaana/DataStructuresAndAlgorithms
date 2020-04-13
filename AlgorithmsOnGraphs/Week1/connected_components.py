c8d3c60a59417c33b3678b72a0655f9c74813d76
thioaana <i.anastasiadis@ergohellas.gr>
2020-04-14 00:59:55 +0300

    Week 1 completed

--- AlgorithmsOnGraphs/Week1/connected_components.py
+++ AlgorithmsOnGraphs/Week1/connected_components.py
@@ -2,18 +2,30 @@
 
 import sys
 
-
 def number_of_components(adj):
-    result = 0
-    #write your code here
-    return result
+    def Explore(a) :
+        visited[a] = True
+        for i in adj[a] :
+            if not visited[i] :
+                Explore(i)
+
+    visited = len(adj) * [False]
+    cc = 0
+    for v in range(len(adj)) :
+        if not visited[v] :
+            Explore(v)
+            cc += 1
+
+    return cc
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    inp = input().split()
+    n = int(inp[0])
+    m = int(inp[1])
+    edges = []
+    for i in range(m):
+        inp = input().split()
+        edges.append((int(inp[0]), int(inp[1])))
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
