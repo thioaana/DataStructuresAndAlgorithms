08dbcbde2056169d7bc4fb7d081a84e0533e93e9
thioaana <i.anastasiadis@ergohellas.gr>
2020-06-14 22:28:54 +0300

    Week2 PalnParty.py completed

--- AdvancedAlgorithmsAndComplexity/Week4NPCompleteness/PlanParty.py
+++ AdvancedAlgorithmsAndComplexity/Week4NPCompleteness/PlanParty.py
@@ -12,7 +12,7 @@ class Vertex:
     def __init__(self, weight):
         self.weight = weight
         self.children = []
-
+        self.D = sys.maxsize
 
 def ReadTree():
     size = int(input())
@@ -23,25 +23,35 @@ def ReadTree():
         tree[b - 1].children.append(a - 1)
     return tree
 
+def FunParty(tree, vertex, parent) :
+    if len(tree[vertex].children) == 1 and parent != -1:
+        tree[vertex].D = tree[vertex].weight
+    else :
+        m1 = tree[vertex].weight
+        for u in tree[vertex].children : # For all children
+            if u != parent :
+                for v in tree[u].children:   # For all grand children
+                    if v != vertex :
+                        m1 = m1 + tree[v].D
+        m0 = 0
+        for u in tree[vertex].children:  # For all children
+            if u != parent:
+                m0 = m0 + tree[u].D
+        tree[vertex].D = max(m0, m1)
 
 def dfs(tree, vertex, parent):
     for child in tree[vertex].children:
         if child != parent:
             dfs(tree, child, vertex)
-
-    # This is a template function for processing a tree using depth-first search.
-    # Write your code here.
-    # You may need to add more parameters to this function for child processing.
-
+        # else :
+    FunParty(tree, vertex, parent)
 
 def MaxWeightIndependentTreeSubset(tree):
     size = len(tree)
     if size == 0:
         return 0
     dfs(tree, 0, -1)
-    # You must decide what to return.
-    return 0
-
+    return tree[0].D
 
 def main():
     tree = ReadTree();
