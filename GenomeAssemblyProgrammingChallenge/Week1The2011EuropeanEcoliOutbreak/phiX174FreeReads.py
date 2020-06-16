250fdedc3f7b9bdb2b2fc14c7d4550190a9c701d
thioaana <i.anastasiadis@ergohellas.gr>
2020-06-17 00:12:19 +0300

    replace phiX174FreeReads.py

--- GenomeAssemblyProgrammingChallenge/Week1The2011EuropeanEcoliOutbreak/phiX174FreeReads.py
+++ GenomeAssemblyProgrammingChallenge/Week1The2011EuropeanEcoliOutbreak/phiX174FreeReads.py
@@ -1,4 +1,4 @@
-# pytthon 3
+#uses python3
 
 def getCommonPrefixOrSuffix(r,g, l):#(r, k, l) :
     # Find common substring
@@ -9,7 +9,6 @@ def getCommonPrefixOrSuffix(r,g, l):#(r, k, l) :
             break
         c1 -= 1
 
-
     # From prefix of g and suffix of l
     c2 = min(len(g), len(r[l]))
     while c2 > 0:
@@ -17,31 +16,10 @@ def getCommonPrefixOrSuffix(r,g, l):#(r, k, l) :
             break
         c2 -= 1
 
-
-    # # Find common substring
-    # # From suffix of k and prefix of l
-    # c1 = min(len(r[k]), len(r[l]))
-    # while c1 > 0 :
-    #     if r[l].startswith(r[k][-c1:]) :
-    #         break
-    #     c1 -= 1
-    # # c1 += 1
-    #
-    # # From prefix of k and suffix of l
-    # c2 = min(len(r[k]), len(r[l]))
-    # while c2 > 0:
-    #     if r[l].endswith(r[k][:c2]):
-    #         break
-    #     c2 -= 1
-    # # c2 += 1
-
     return c1, c2
 
-
 if __name__ == "__main__" :
-    # r = ["abcdef", "def1"]
-    # c = getCommonPrefixOrSuffix(r, 0, 1)
-    numReads = 5#1618
+    numReads = 1618
     reads = []
     for i in range(numReads) :
         reads.append(input().strip())
@@ -49,12 +27,10 @@ if __name__ == "__main__" :
     visited = [False] * numReads
     visited[0]= True
     genome = reads[0]
-    for i in range(numReads-1) :
-        # if visited[i]: continue
-        # visited[i] = True
-        maxSP = [0, -1] #[maximum S-P, index  of P].
-        maxPS = [0, -1] #[maximum P-S, index  of S].
-        for j in range(numReads):#(i + 1, numReads) :
+    for i in range(numReads-2) :
+        maxSP = [0, -1]
+        maxPS = [0, -1]
+        for j in range(numReads):
             if visited[j] : continue
 
             cSP, cPS = getCommonPrefixOrSuffix(reads, genome, j)
@@ -69,4 +45,9 @@ if __name__ == "__main__" :
         else :
             genome = reads[maxPS[1]][:maxPS[0]-1] + genome
             visited[maxPS[1]] = True
+    falseIndex = visited.index(False)
+    cSP, cPS = getCommonPrefixOrSuffix(reads, genome, falseIndex)
+    genome = genome + reads[falseIndex][cSP:-cPS]
+
+
     print(genome)
