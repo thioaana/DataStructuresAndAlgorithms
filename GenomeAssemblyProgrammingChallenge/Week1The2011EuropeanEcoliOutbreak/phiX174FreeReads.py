e3099c79819dfe18e3d14696adcab54d580a8c98
thioaana <i.anastasiadis@ergohellas.gr>
2020-06-18 20:54:45 +0300

    Week1 phiX174FreeReeds.py completed

--- GenomeAssemblyProgrammingChallenge/Week1The2011EuropeanEcoliOutbreak/phiX174FreeReads.py
+++ GenomeAssemblyProgrammingChallenge/Week1The2011EuropeanEcoliOutbreak/phiX174FreeReads.py
@@ -1,22 +1,15 @@
 #uses python3
 
-def getCommonPrefixOrSuffix(r,g, l):#(r, k, l) :
+def getCommonSP(r, k, l, exLap) :
     # Find common substring
     # From suffix of g and prefix of l
-    c1 = min(len(g), len(r[l]))
+    m = min(len(r[k]), len(r[l]))
+    c1 = m
     while c1 > 0 :
-        if r[l].startswith(g[-c1:]) :
-            break
+        if c1 <= exLap : return 0
+        if r[l][:c1] == r[k][-c1:] : break
         c1 -= 1
-
-    # From prefix of g and suffix of l
-    c2 = min(len(g), len(r[l]))
-    while c2 > 0:
-        if r[l].endswith(g[:c2]):
-            break
-        c2 -= 1
-
-    return c1, c2
+    return c1
 
 if __name__ == "__main__" :
     numReads = 1618
@@ -27,27 +20,22 @@ if __name__ == "__main__" :
     visited = [False] * numReads
     visited[0]= True
     genome = reads[0]
-    for i in range(numReads-2) :
+    i =0
+    numForSearch = numReads - 1
+    while numForSearch > 0 :
         maxSP = [0, -1]
-        maxPS = [0, -1]
         for j in range(numReads):
             if visited[j] : continue
 
-            cSP, cPS = getCommonPrefixOrSuffix(reads, genome, j)
+            cSP = getCommonSP(reads, i, j, maxSP[0])
             if cSP > maxSP[0] :
                 maxSP = [cSP, j]
-            if cPS > maxPS[0] :
-                maxPS = [cPS, j]
 
-        if maxSP[0] >= maxPS[0] :
-            genome = genome + reads[maxSP[1]][maxSP[0]:]
-            visited[maxSP[1]] = True
-        else :
-            genome = reads[maxPS[1]][:maxPS[0]-1] + genome
-            visited[maxPS[1]] = True
-    falseIndex = visited.index(False)
-    cSP, cPS = getCommonPrefixOrSuffix(reads, genome, falseIndex)
-    genome = genome + reads[falseIndex][cSP:-cPS]
+        genome = genome + reads[maxSP[1]][maxSP[0]:]
+        visited[maxSP[1]] = True
+        i = maxSP[1]
+        numForSearch -= 1
 
-
+    cPS = getCommonSP(reads, maxSP[1], 0, 0)
+    genome = genome[:-cPS]
     print(genome)
